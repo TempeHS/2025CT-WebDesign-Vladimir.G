97c133e7451d0c180a1166fe4de28183de0dce04
Vladimir-Grebenets <159206796+Vladimir-Grebenets@users.noreply.github.com>
2024-07-03 12:49:52 +1000

    People and equipment devolping
    adding code and making every image black and white.
    Fixing popover

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
