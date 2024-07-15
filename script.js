8db94a21b4b7e09f2b5e91bd7a13fe39095f6fdc
Vladimir Grebenets <vladimir.grebenets181@education.nsw.gov.au>
2024-07-15 17:48:41 +1000

    added elements to PaE

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function myFunction() {
+function toggleDarkMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
+$(function () {
+   $('[data-toggle="popover"]').popover()
+})
